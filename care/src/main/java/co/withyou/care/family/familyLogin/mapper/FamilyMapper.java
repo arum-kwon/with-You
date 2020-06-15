6ece1501d0d62db8bf36bd55df5c93f365a430dc
USER <USER@DESKTOP-9NPUVBE>
2020-06-15 21:43:46 +0900

    재선) 보호자 로그인 파일들 다시 수정

--- care/src/main/java/co/withyou/care/family/familyLogin/mapper/FamilyMapper.java
+++ care/src/main/java/co/withyou/care/family/familyLogin/mapper/FamilyMapper.java
@@ -6,8 +6,4 @@ import co.withyou.care.family.familyLogin.service.FamilyService;
 
 public interface FamilyMapper extends FamilyService {
 
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> branch 'master' of https://github.com/arum-kwon/with-You.git
--- care/src/main/java/co/withyou/care/family/familyLogin/serviceImpl/FamilyServiceImpl.java
+++ care/src/main/java/co/withyou/care/family/familyLogin/serviceImpl/FamilyServiceImpl.java
@@ -18,26 +18,5 @@ public class FamilyServiceImpl implements FamilyService {
 		// TODO Auto-generated method stub
 		return famloginmap.getSelect(vo);
 	}
-}
-package co.withyou.care.family.familyLogin.serviceImpl;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import co.withyou.care.family.familyLogin.mapper.FamilyMapper;
-import co.withyou.care.family.familyLogin.service.FamilyService;
-import co.withyou.care.family.familyLogin.service.FamilyVO;
-
-@Service("familyLoginService")
-public class FamilyServiceImpl implements FamilyService {
-
-	@Autowired
-	private FamilyMapper famloginmap;
-	
-	@Override
-	public FamilyVO getSelect(FamilyVO vo) throws Exception {
-		// TODO Auto-generated method stub
-		return famloginmap.getSelect(vo);
-	}
 
 }
