d51d4223dbaa8b9d16b4bfc1d5c43887df93f4a9
정성훈 <@DESKTOP-INPCNAD.mshome.net>
2020-06-22 00:35:28 +0900

    스케쥴관리 데이터가져오기 ing by 성훈

---
+++ care/src/main/java/co/withyou/care/helper/schedule/web/ScheduleController.java
@@ -0,0 +1,30 @@
+package co.withyou.care.helper.schedule.web;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.withyou.care.helper.schedule.service.ApplyVo;
+import co.withyou.care.helper.schedule.service.ScheduleService;
+
+@Controller
+public class ScheduleController {
+	@Autowired
+	private ScheduleService scheduleService;
+	
+	@RequestMapping("/schedule.do")
+	HashMap<String, Object> myPlan(HttpServletRequest request, ApplyVo vo) throws Exception {
+		HashMap<String, Object> map = new HashMap<String, Object>();
+		HttpSession session = request.getSession();
+		List<Map> schedule = scheduleService.getSchedule(vo);
+		map.put("shedule", schedule);
+		return map;
+	}
+}
