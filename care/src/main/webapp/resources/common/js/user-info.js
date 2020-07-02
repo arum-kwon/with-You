fff37bdec7dbab6699b99ef717ea485f01fb9baa
User <User@YD01-15>
2020-07-02 19:47:25 +0900

    권아름 주소 팝업 모바일팝업 설정으로 바꿈

--- care/src/main/webapp/resources/common/js/user-info.js
+++ care/src/main/webapp/resources/common/js/user-info.js
@@ -1,7 +1,9 @@
 //주소 api 샘플은  views/common/addr/jusoPopup 참고
+document.domain = "abc.go.kr";
 function jusoPopup(index){
 	// 주소검색을 수행할 팝업 페이지를 호출합니다.=
 	jusoIndex = index;
+	//window.MyApp.setAddress();
 	var pop = window.open("jusoPopup.do","pop","width=570,height=420, scrollbars=yes, resizable=yes"); 
 }
 //주소 팝업에서 콜백됨
